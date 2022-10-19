f13af492a1d528bd15f6da652031e3bdf7e49897
nelson ekango <ekannel@yahoo.fr>
2022-10-19 13:28:22 +0200

    prima commit idraulica

---
+++ idraulica/Ripartitore.java
@@ -0,0 +1,19 @@
+package idraulica;
+
+
+public class Ripartitore extends Split {
+
+	public Ripartitore(String nome, int num){
+		super(nome);
+		// TODO: completare
+	}
+	
+	public int getNumeroUscite(){
+		// TODO: completare
+		return -1;
+	}
+	
+	public void setProporzioni(double[] proporzioni){
+		// TODO: completare
+	}
+}
---
+++ idraulica/Split.java
@@ -0,0 +1,13 @@
+package idraulica;
+
+public class Split extends Elemento {
+
+	public Split(String nome) {
+		super(nome);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void connetti(Elemento elem, int uscita){
+		// TODO: completare
+	}
+}
